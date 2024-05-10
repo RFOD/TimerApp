b8e539f149b563d158a25414d2b478ca1bebc1ec
Rain <6rainforced9@gmail.com>
2024-05-10 11:44:39 +0300

    Finished Stopwath with oop JavaScript

--- index.js
+++ index.js
@@ -1,52 +1,83 @@
-function Stopwatch()
+let active = false;
+let duration = 0;
+let start = 0;
+let intervalId = null;
+let displayedTime = 0;
+let sw = 
 {
-    let active = false;
-    let duration = 0;
-    let start = 0;
-    let intervalId = null;
-    Object.defineProperty(this, 'duration', {
-        get: () =>
-        {
-            return duration;
-        }
-    })
-    this.reset = () =>
+    reset: () =>
     {
-        start = 0;
-        end = 0;
-        duration = 0;
-    }
-    this.start = () =>
+        if(active)
+            {
+                document.querySelector(".stopwatch").textContent = displayedTime;
+                active = false;
+                clearInterval(intervalId);
+                start = 0;
+                end = 0;
+                duration = 0;
+                displayedTime = "0.000 sec";
+            }
+        else{
+            document.querySelector(".stopwatch").textContent = "0.000 sec";
+            active = false;
+            clearInterval(intervalId);
+            start = 0;
+            end = 0;
+            duration = 0;
+            displayedTime = "0.000 sec";
+        }
+    },
+    start: () =>
     {   
         if(start >= 0){
             if (!active){
                 active = true;
+                document.querySelector(".stopwatch").textContent = "";
                 start = Date.now();
                 intervalId = setInterval(() => {
                     let now = Date.now();
                     duration += (now - start) / 1000;
                     start = now;
-                    console.log('Duration: ' + duration.toFixed(3) + ' seconds');
-                }, 50);
+                    displayedTime = (`${duration.toFixed(3)}  sec`);
+                    document.querySelector(".stopwatch").textContent = displayedTime;
+                }, 25);
             }
             else{
-            alert("The Stopwatch has been already started")
-            }
-    }  
-    }
-    this.stop = () =>
+                throw new Error("Stopwatch already active Mr.Fish!");
+            } 
+        }
+        
+    },
+    stop: () =>
     {
         if(active)
             {
+                document.querySelector(".stopwatch").textContent = "";
                 active = false;
                 end = Date.now(); 
                 duration += (end - start) / 1000;
                 clearInterval(intervalId);
+                displayedTime = (`${duration.toFixed(3)}  sec`);
+                document.querySelector(".stopwatch").textContent = displayedTime;
             }else{
-                alert("Stopwatch has not been started yet")
+                throw new Error("Stopwatch already stopped Mr.Fish!!");
             }
         
     }  
-}
 
-const sw = new Stopwatch;
+}
+Object.defineProperty(sw, 'duration', {
+    get: () =>
+    {
+        return duration;
+    }
+})
+
+let stopBtn = document.querySelector(".stop");
+stopBtn.addEventListener("click", sw.stop)
+
+let resetBtn = document.querySelector(".reset");
+resetBtn.addEventListener("click", sw.reset)
+
+let startBtn = document.querySelector(".start");
+startBtn.addEventListener("click", sw.start)
